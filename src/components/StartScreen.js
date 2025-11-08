f7656b7e8a215b4baf16ad4715119099b1719503
Ahmed Baraa <ahmedbaraa009@gmail.com>
2025-11-08 16:00:36 +0200

    start screen

---
+++ src/components/StartScreen.js
@@ -0,0 +1,19 @@
+function StartScreen({ numQuestions, dispatch }) {
+  return (
+    <div className="start">
+      <h2>Welcome to react quiz</h2>
+      <h3>{numQuestions} questions to test your react mastery</h3>
+      <button
+        className="btn btn-ui"
+        onClick={() => {
+          console.log('button clicked');
+          dispatch({ type: 'start' });
+        }}
+      >
+        let's start
+      </button>
+    </div>
+  );
+}
+
+export default StartScreen;
